3684b58722bf61c703e00488b502bc4ed96f2685
ĐALÂY HOÀI LĨNH <hoailinh1862k3@gmail.com>
2026-04-16 06:55:17 +0700

    test: add CandlestickPainter shouldRepaint widget test

---
+++ test/widget/market/candlestick_painter_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_roadmap_foundation/features/market/domain/entities/candle_data.dart';
+import 'package:flutter_roadmap_foundation/features/market/presentation/widgets/candlestick_painter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final time = DateTime(2026, 4, 16, 5, 0, 0);
+
+  final candle1 = CandleData(
+    time: time,
+    open: 100.0,
+    high: 105.0,
+    low: 98.0,
+    close: 102.0,
+  );
+
+  final candle2 = CandleData(
+    time: time.add(const Duration(minutes: 1)),
+    open: 102.0,
+    high: 108.0,
+    low: 101.0,
+    close: 106.0,
+  );
+
+  group('CandlestickPainter.shouldRepaint', () {
+    test('Trả về false khi cùng data reference', () {
+      final data = [candle1, candle2];
+      final painter1 = CandlestickPainter(data: data);
+      final painter2 = CandlestickPainter(data: data);
+
+      expect(painter1.shouldRepaint(painter2), false);
+    });
+
+    test('Trả về true khi data reference khác nhau', () {
+      final painter1 = CandlestickPainter(data: [candle1, candle2]);
+      final painter2 = CandlestickPainter(data: [candle1, candle2]);
+
+      expect(painter1.shouldRepaint(painter2), true);
+    });
+  });
+}
